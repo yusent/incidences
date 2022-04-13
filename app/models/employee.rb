fe2ecc8d25d0b6fd8f5130f4cd9596490a28b922
Yusent Chig <yusent@protonmail.com>
2022-04-12 21:55:42 -0700

    Add contracts association to employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ApplicationRecord
+  has_many :contracts
+  has_one :active_contract, -> { where(active: true) }, class_name: "Contract"
+
   validates_presence_of :first_name, :last_name, :rfc, :date_registered,
     :imss_number
   validates_uniqueness_of :rfc, :imss_number
