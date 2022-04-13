97783189aafd657a3c603ec886056b14bae72a87
Yusent Chig <yusent@protonmail.com>
2022-04-12 20:59:20 -0700

    Make RFC_REGEX reject values containing but not limiting to an RFC

--- app/validators/rfc_validator.rb
+++ app/validators/rfc_validator.rb
@@ -1,5 +1,5 @@
 class RfcValidator < ActiveModel::EachValidator
-  RFC_REGEX = /[A-Z&Ñ]{3,4}\d\d(0\d|1[012])(0\d|[12]\d|3[01])[A-Z\d]{2}[\dA]/
+  RFC_REGEX = /^[A-Z&Ñ]{3,4}\d\d(0\d|1[012])(0\d|[12]\d|3[01])[A-Z\d]{2}[\dA]$/
 
   def validate_each(record, attribute, value)
     unless value.nil? || value =~ RFC_REGEX
