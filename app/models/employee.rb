8ff797a3cc5f77fc9041eadeb8779b28bab9dcef
Yusent Chig <yusent@protonmail.com>
2022-04-12 19:43:09 -0700

    Add presence and uniqueness validations to Employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+  validates_presence_of :first_name, :last_name, :rfc, :date_registered,
+    :imss_number
+  validates_uniqueness_of :rfc, :imss_number
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:model) { FactoryBot.build :employee }
+
+  describe "validations" do
+    # Presence validations
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:rfc) }
+    it { should validate_presence_of(:date_registered) }
+    it { should validate_presence_of(:imss_number) }
+
+    # Uniqueness validations
+    it { should validate_uniqueness_of(:rfc) }
+    it { should validate_uniqueness_of(:imss_number) }
+  end
 end
