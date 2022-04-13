96201dda37193bf870a76ce37a2a42f556aadfc3
Yusent Chig <yusent@protonmail.com>
2022-04-13 14:03:55 -0700

    Use Faker in Incidence's factory

--- spec/factories/incidences.rb
+++ spec/factories/incidences.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :incidence do
-    employee { nil }
-    incidence_type { 1 }
-    concept { 1 }
-    start_date { "2022-04-13" }
-    end_date { "2022-04-13" }
-    amount { 1.5 }
+    association :employee
+    incidence_type { %i[deductions perceptions].sample }
+    concept { %i[illness absence delay vacation extrahours extraday].sample }
+    start_date { rand(4..6).days.ago.to_date }
+    end_date { rand(3).days.ago.to_date }
+    amount { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
   end
 end
