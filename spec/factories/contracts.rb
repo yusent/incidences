35097694cd4663e4e907edc52b5c41a992631496
Yusent Chig <yusent@protonmail.com>
2022-04-12 22:13:50 -0700

    Use Faker in Contracts' factory

--- spec/factories/contracts.rb
+++ spec/factories/contracts.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :contract do
-    employee { nil }
-    gross_salary { 1.5 }
-    payroll_type { 1 }
-    active { "" }
+    association :employee
+    gross_salary { Faker::Number.decimal(l_digits: 4, r_digits: 2) }
+    payroll_type { %i[weekly biweekly monthly].sample }
+    active { true }
   end
 end
