ef2d4f421a8278acd18979f823cf894a1f032393
Yusent Chig <yusent@protonmail.com>
2022-04-12 22:13:39 -0700

    Deactive other active contracts after activating

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,4 +2,13 @@ class Contract < ApplicationRecord
   belongs_to :employee
 
   enum payroll_type: %i[weekly biweekly monthly]
+
+  after_save :deactive_other_contracts_if_active
+
+  def deactive_other_contracts_if_active
+    self.class
+      .where(employee: employee, active: true)
+      .where.not(id: id)
+      .update_all(active: false) if active?
+  end
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe Contract, type: :model do
     it { should define_enum_for(:payroll_type)
       .with_values(%i[weekly biweekly monthly]) }
   end
+
+  describe "after save" do
+    it "should only be one active contract per employee" do
+      model.update_attributes active: true
+      FactoryBot.create :contract, employee: model.employee, active: true
+      expect(Contract.find(model.id).active).to be(false)
+    end
+  end
 end
