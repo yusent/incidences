bc0b560646673b7d3076833a00ba7c538b6411fc
Yusent Chig <yusent@protonmail.com>
2022-04-12 21:45:20 -0700

    Set payroll_type as an enum attribute

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,3 +1,5 @@
 class Contract < ApplicationRecord
   belongs_to :employee
+
+  enum payroll_type: %i[weekly biweekly monthly]
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Contract, type: :model do
   describe "associations" do
     it { should belong_to(:employee) }
   end
+
+  describe "attributes" do
+    it { should define_enum_for(:payroll_type)
+      .with_values(%i[weekly biweekly monthly]) }
+  end
 end
