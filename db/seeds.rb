b3b80e4d0c204377f63071579f793676d7805f2b
Yusent Chig <yusent@protonmail.com>
2022-04-13 20:00:14 -0700

    Add seed data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+employees = FactoryBot.create_list :employee, 30
+employees.each { |e| FactoryBot.create :contract, employee: e }
