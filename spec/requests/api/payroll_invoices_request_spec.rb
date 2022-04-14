ad41d469a0853c64df7890a1aae956485478adab
Yusent Chig <yusent@protonmail.com>
2022-04-13 18:10:18 -0700

    Test return total calculation in invoice creation endpoint

--- spec/requests/api/payroll_invoices_request_spec.rb
+++ spec/requests/api/payroll_invoices_request_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe Api::PayrollInvoicesController, type: :request do
   content_type = "application/json"
 
+  let(:employee) { FactoryBot.create :employee }
+
   let(:valid_attributes) do
     FactoryBot
-      .build(:payroll_invoice, employee: FactoryBot.create(:employee))
+      .build(:payroll_invoice, employee: employee)
       .attributes
   end
 
@@ -23,6 +25,19 @@ RSpec.describe Api::PayrollInvoicesController, type: :request do
         post api_payroll_invoice_url, params: valid_attributes, as: :json
         expect(response).to have_http_status(:created)
         expect(response.content_type).to match(a_string_including(content_type))
+      end
+
+      it "returns the calculated total" do
+        FactoryBot.create :contract, active: true, employee: employee,
+          gross_salary: 144
+        FactoryBot.create :incidence, incidence_type: :deductions,
+          amount: 1.1, employee: employee
+        FactoryBot.create :incidence, incidence_type: :perceptions,
+          amount: 2.2, employee: employee
+
+        post api_payroll_invoice_url, params: valid_attributes, as: :json
+        response_body = JSON.parse response.body
+        expect(response_body["total"]).to eq(145.1)
       end
     end
 
